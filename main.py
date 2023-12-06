91fc7f369b7fc047679428c06f017f1f67345a7d
Aleksey Malyshkin <5048818@gmail.com>
2023-12-06 20:52:26 +0300

    add task_1

--- main.py
+++ main.py
@@ -22,5 +22,48 @@ def read_recipes(file_name):
           i += 1
   return recipes
 
+file_name = 'recipes.txt'
+recipes = read_recipes(file_name)
 
+for recipe_name, recipe_info in recipes.items():
+  num_ingredients = recipe_info['num_ingredients']
+  ingredients = recipe_info['ingredients']
+  print(f"\n{recipe_name}:")
+  print(f"Количество ингредиентов: {num_ingredients}")
+  for ingredient, info in ingredients.items():
+      print(f"- {ingredient}: {info['quantity']} {info['unit']}")
 
+############### Задача№1 ################
+
+def read_recipes(file_name):
+  cook_book = {}
+  with open(file_name, 'r', encoding='utf-8') as file:
+      lines = file.readlines()
+      i = 0
+      while i < len(lines):
+          recipe_name = lines[i].strip()
+          i += 1
+          num_ingredients = int(lines[i])
+          i += 1
+          ingredients = []
+          for j in range(num_ingredients):
+              ingredient_info = lines[i].strip().split('|')
+              ingredient_name = ingredient_info[0].strip()
+              quantity = float(ingredient_info[1])
+              measure = ingredient_info[2].strip()
+              ingredients.append({
+                  'ingredient_name': ingredient_name,
+                  'quantity': quantity,
+                  'measure': measure
+              })
+              i += 1
+          cook_book[recipe_name] = ingredients
+          i += 1
+  return cook_book
+
+cook_book = read_recipes(file_name)
+
+# Вывод словаря cook_book для проверки
+print()
+print(cook_book)
+print()
