52b4d40af2ec4fe454c5cd2771b320f01b3cbbd8
Aleksey Malyshkin <5048818@gmail.com>
2023-12-06 21:03:30 +0300

    add task_2

--- main.py
+++ main.py
@@ -63,7 +63,29 @@ def read_recipes(file_name):
 
 cook_book = read_recipes(file_name)
 
-# Вывод словаря cook_book для проверки
-print()
 print(cook_book)
-print()
+
+################## Задача №2 #######################
+
+def get_shop_list_by_dishes(dishes, person_count, cook_book):
+  shop_list = {}
+  for dish in dishes:
+      if dish in cook_book:
+          for ingredient in cook_book[dish]:
+              ingredient_name = ingredient['ingredient_name']
+              quantity = ingredient['quantity'] * person_count
+              measure = ingredient['measure']
+              if ingredient_name not in shop_list:
+                  shop_list[ingredient_name] = {'quantity': quantity, 'measure': measure}
+              else:
+                  shop_list[ingredient_name]['quantity'] += quantity
+      else:
+          print(f"Блюдо '{dish}' отсутствует в кулинарной книге.")
+  return shop_list
+
+dishes = ['Запеченный картофель', 'Омлет']
+person_count = 2
+
+shop_list = get_shop_list_by_dishes(dishes, person_count, cook_book)
+
+print(shop_list)
